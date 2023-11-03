2553c6202bfd3a2e2baa247f981974a201f744a3
Jalə <jal@Jale-MacBook-Air.local>
2023-11-03 14:50:15 +0400

    reviewed

--- Academy.Core/Repositories/IStudentRepository.cs
+++ Academy.Core/Repositories/IStudentRepository.cs
@@ -1,11 +1,11 @@
 ﻿using System;
-using Academy.Core.Models;
-
 namespace Academy.Core.Repositories
 {
-    public interface IStudentRepository : IRepository<Student>
-    {
-        Task<Student> GetAsync(Func<object, bool> value);
-    }
+	public class IStudentRepository
+	{
+		public IStudentRepository()
+		{
+		}
+	}
 }
 
